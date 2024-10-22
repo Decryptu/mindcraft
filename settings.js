52e4aa38958928a233aefad6b9fe185431e7d87f
Decrypt <louis.vicomte@protonmail.com>
2024-10-22 16:03:25 +0200

    config

--- settings.js
+++ settings.js
@@ -12,17 +12,17 @@ export default
         // more than 1 profile will require you to /msg each bot indivually
     ],
     "load_memory": false, // load memory from previous session
-    "init_message": "Say hello world and your name", // sends to all on spawn
+    "init_message": "Dis bonjour au monde et ton nom.", // sends to all on spawn
 
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
-    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
+    "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
 
     "max_messages": 15, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
-    "verbose_commands": true, // show full command syntax
+    "verbose_commands": false, // show full command syntax
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
 }
 
